43e79d460d7487ec2fd046dc6e86ec424726b175
nishantmc <nishantmc@gmail.com>
2012-12-16 20:07:13 -0600

    Update AS_Exec.v

--- AS_Exec.v
+++ AS_Exec.v
@@ -1,24 +1,32 @@
 
 
-Load AS_Int.
+Load Integers.
+Load AS_State.
 Load AS_Int_Impl.
+Load AS_Int.
 
+(* The execution of single instruction *)
 Definition exec :instruction -> state -> state :=
   fun inst state => match state with
   | createState stack pc reg_0 reg_1 reg_2 reg_3 => 
       match inst with
-      |inst_pushbyte n => exec_pushbyte (data n false true) state
+      |inst_pushbyte n => exec_pushbyte (data n false false) state
       |inst_pop => exec_pop state
-      |inst_getlocal_0 => getLocal state 0
-      |inst_getlocal_1 => getLocal state 1
-      |inst_getlocal_2 => getLocal state 2
-      |inst_getlocal_3 => getLocal state 3
-      |inst_add        => addFromState state
+      |inst_getlocal_0 => exec_getLocal state 0
+      |inst_getlocal_1 => exec_getLocal state 1
+      |inst_getlocal_2 => exec_getLocal state 2
+      |inst_getlocal_3 => exec_getLocal state 3
+      |inst_setlocal_0 => exec_setLocal state 0
+      |inst_setlocal_1 => exec_setLocal state 1
+      |inst_setlocal_2 => exec_setLocal state 2
+      |inst_setlocal_3 => exec_setLocal state 3
+      |inst_add        => exec_add state
       |inst_convert_i  => convertIFromState state
       |_ => state
       end
   end.
 
+(* The execution of list of instructions *)
 Fixpoint startExec (inst_seq:list instruction) (s:state) : state := 
   match inst_seq with
  | nil => s
